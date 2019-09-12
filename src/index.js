9a6f795a36de77b9b16f287fc85ad08e94ece7cb
Greyflanel <richardlovelace811@gmail.com>
2019-09-12 13:51:42 -0500

    Added attributes to jsx in order to pass more properties from parent to child.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="What's the matter with you?" />
-      <CommentDetail author="Jane" timeAgo="Today at 6:23AM" commentText="He'll knock you into " />
-      <CommentDetail author="Alex" timeAgo="Today at 11:15AM" commentText="" />
-      <CommentDetail author="James" timeAgo="Yesterday at 7:56" commentText="" />
-      <CommentDetail author="Michael" timeAgo="Yesterday at 5:48" commentText="" />
-      
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="One question at a time." />
+      <CommentDetail author="Jane" timeAgo="Today at 6:23AM" commentText="We were never born and we will never die." />
+      <CommentDetail author="Alex" timeAgo="Today at 11:15AM" commentText="Of the essence, there is no such thing as death" />
+      <CommentDetail author="James" timeAgo="Yesterday at 7:56" commentText="The soul is eternal, you just change form." />
+      <CommentDetail author="Michael" timeAgo="Yesterday at 5:48" commentText="Then you come back with a new face on." />
     </div>
   );
 };
