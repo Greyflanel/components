ef97d9d0d8ce3181ba707931af03e189ff6179f6
Greyflanel <richardlovelace811@gmail.com>
2019-09-12 15:03:06 -0500

    I believe this project is finished.

--- src/index.js
+++ src/index.js
@@ -7,24 +7,57 @@ import faker from "faker";
 const App = () => {
   return (
     <div className="ui container comments">
-        <ApprovalCard>
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="One question at a time."avatar={faker.image.avatar()}  />
+       <ApprovalCard> 
+          <div> 
+              <h4>Warning!</h4>
+              Are you sure you want to do this?
+           </div>
+           </ApprovalCard> 
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          commentText="One question at a time."
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
 
       <ApprovalCard>
-      <CommentDetail author="Jane" timeAgo="Today at 6:23AM" commentText="We were never born and we will never die." avatar={faker.image.avatar()} />
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 6:23AM"
+          commentText="We were never born and we will never die."
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
+
       <ApprovalCard>
-      <CommentDetail author="Alex" timeAgo="Today at 11:15AM" commentText="Of the essence, there is no such thing as death" avatar={faker.image.avatar()} />
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 11:15AM"
+          commentText="Of the essence, there is no such thing as death"
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
+
       <ApprovalCard>
-      <CommentDetail author="James" timeAgo="Yesterday at 7:56" commentText="The soul is eternal, you just change form." avatar={faker.image.avatar()} />
+        <CommentDetail
+          author="James"
+          timeAgo="Yesterday at 7:56"
+          commentText="The soul is eternal, you just change form."
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
+
       <ApprovalCard>
-      <CommentDetail author="Michael" timeAgo="Yesterday at 5:48" commentText="Then you come back with a new face on." avatar={faker.image.avatar()} />
+        <CommentDetail
+          author="Michael"
+          timeAgo="Yesterday at 5:48"
+          commentText="Then you come back with a new face on."
+          avatar={faker.image.avatar()}
+        />
       </ApprovalCard>
     </div>
-    
   );
 };
 
